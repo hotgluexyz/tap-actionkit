811056b85cd716f99061832efd039fb08201c280
arilton <arilton@hotglue.io>
2024-11-27 15:41:25 -0300

    fix to match user schema. also, it is ready to sync

--- tap_actionkit/client.py
+++ tap_actionkit/client.py
@@ -3,41 +3,46 @@
 from __future__ import annotations
 
 import typing as t
-from requests.auth import HTTPBasicAuth
+from requests.auth import _basic_auth_str
 from singer_sdk.streams import RESTStream
 
 class ActionKitStream(RESTStream):
     """ActionKit stream class."""
 
-    records_jsonpath = "$[*]"
-    next_page_token_jsonpath = "$.next_page"
+    records_jsonpath = "$.objects[*]"
+    next_page_token_jsonpath = "$.meta.next"
+    limit = 100
 
     @property
     def url_base(self) -> str:
         hostname = self.config.get("hostname")
         if not hostname:
             raise ValueError("'hostname' must be provided in the config. Please check your configuration.")
-        return f"https://{hostname}.actionkit.com/rest/v1/"
+        return f"https://{hostname}.actionkit.com/rest/v1"
 
     @property
-    def authenticator(self) -> HTTPBasicAuth:
+    def authenticator(self) -> None:
+        return None
+
+    @property
+    def http_headers(self) -> dict:
         username = self.config.get("username")
         password = self.config.get("password")
         
         if not username or not password:
             raise ValueError("Both username and password must be provided in the config. Please check your configuration.")
-            
-        return HTTPBasicAuth(username=username, password=password)
 
-    @property
-    def http_headers(self) -> dict:
-        return {}
+        return {"Authorization": _basic_auth_str(username, password)}
 
     def get_url_params(self, context: t.Optional[dict], next_page_token: t.Optional[str]) -> t.Dict[str, t.Any]:
         params: dict = {}
         if next_page_token:
-            params["page"] = next_page_token
+            offset = next_page_token.split("_offset=")[-1].split("&")[0]
+            params["_offset"] = offset
         if self.replication_key:
-            params["sort"] = "asc"
-            params["order_by"] = self.replication_key
+            params["order_by"] = self.replication_key # if you want the reverse order, use -replication_key
+        if self.limit:
+            params["_limit"] = self.limit
+        params["format"] = "json"
+
         return params
--- tap_actionkit/streams.py
+++ tap_actionkit/streams.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-import typing as t
-
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_actionkit.client import ActionKitStream
@@ -13,39 +11,47 @@ class ContactsStream(ActionKitStream):
     """Define custom stream for contacts."""
 
     name = "contacts"
-    path = "/contacts"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
+    path = "/user"
+    primary_keys = ["id"]
     replication_key = None
+    records_jsonpath = "$.objects[*]"
     schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
         th.Property("first_name", th.StringType),
+        th.Property("middle_name", th.StringType),
         th.Property("last_name", th.StringType),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The contact's email address",
-        ),
-        th.Property(
-            "phone_numbers",
-            th.ArrayType(
-                th.ObjectType(
-                    th.Property("type", th.StringType),
-                    th.Property("number", th.StringType),
-                )
-            ),
-            description="List of phone numbers associated with the contact",
-        ),
-        th.Property(
-            "addresses",
-            th.ArrayType(
-                th.ObjectType(
-                    th.Property("line1", th.StringType),
-                    th.Property("city", th.StringType),
-                    th.Property("state", th.StringType),
-                    th.Property("postal_code", th.StringType),
-                    th.Property("country", th.StringType),
-                )
-            ),
-            description="List of addresses associated with the contact",
-        ),
+        th.Property("useroriginal", th.StringType),
+        th.Property("suffix", th.StringType),
+        th.Property("phones", th.ArrayType(th.StringType)),
+        th.Property("updated_at", th.DateTimeType),
+        th.Property("actions", th.StringType),
+        th.Property("prefix", th.StringType),
+        th.Property("orders", th.StringType),
+        th.Property("city", th.StringType),
+        th.Property("zip", th.StringType),
+        th.Property("rand_id", th.IntegerType),
+        th.Property("subscriptionhistory", th.StringType),
+        th.Property("source", th.StringType),
+        th.Property("state", th.StringType),
+        th.Property("location", th.StringType),
+        th.Property("logintoken", th.StringType),
+        th.Property("subscription_status", th.StringType),
+        th.Property("email", th.StringType),
+        th.Property("subscriptions", th.StringType),
+        th.Property("address1", th.StringType),
+        th.Property("address2", th.StringType),
+        th.Property("orderrecurrings", th.StringType),
+        th.Property("eventsignups", th.StringType),
+        th.Property("postal", th.StringType),
+        th.Property("lang", th.StringType),
+        th.Property("plus4", th.StringType),
+        th.Property("fields", th.CustomType({"type": ["object", "string"]})),
+        th.Property("created_at", th.DateTimeType),
+        th.Property("events", th.StringType),
+        th.Property("token", th.StringType),
+        th.Property("usermailings", th.StringType),
+        th.Property("country", th.StringType),
+        th.Property("region", th.StringType),
+        th.Property("resource_uri", th.StringType),
     ).to_dict()
 
